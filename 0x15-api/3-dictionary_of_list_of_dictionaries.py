0a6f56162e2f6f2c9def9cf795d79f6dacc2daf3
Dedeyd70 <dorothyokoto@gmail.com>
2023-11-28 16:35:34 -0800

    fixed pycode

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,11 +8,20 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            usr.get("id"): [{
-                "task": tk.get("title"),
-                "completed": tk.get("completed"),
-                "username": usr.get("username")
-            } for tk in requests.get(url + "todos",
-                                    params={"userId": usr.get("id")}).json()]
-            for usr in users}, jsonfile, indent=2)
+        json.dump(
+            {
+                usr.get("id"): [
+                    {
+                        "task": tk.get("title"),
+                        "completed": tk.get("completed"),
+                        "username": usr.get("username"),
+                    }
+                    for tk in requests.get(
+                        url + "todos", params={"userId": usr.get("id")}
+                    ).json()
+                ]
+                for usr in users
+            },
+            jsonfile,
+            indent=2,
+        )
